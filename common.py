58745f83be169ec6ac5b808594c4278b297c2bbf
brendanforbesdb <brendan.forbes@databricks.com>
2025-11-13 00:33:54 +0000

    common.py to dev

---
+++ common.py
@@ -0,0 +1,5 @@
+def my_function_repo():
+    """
+    A sample function that can be imported and used in other Python files.
+    """
+    print("Hello from my_function in the rio_common repository!")
